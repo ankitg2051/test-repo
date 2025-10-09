47a8cf936cfdedcf7c3e6568eb93152f5d836da5
Your Name <you@example.com>
2025-10-09 21:48:44 +0530

    lesser resources to test pipeline

--- Parent_Module/variable.tf
+++ Parent_Module/variable.tf
@@ -9,35 +9,3 @@ variable "storage" {
 variable "vn" {
   type = any
 }
-
-variable "subnets" {
-  type = any
-}
-
-variable "public_ipss" {
-  type = any
-}
-
-variable "nic_map" {
-  type = any
-}
-
-variable "virtual_machine" {
-  type = any
-}
-
-variable "network_security_group" {
-  type = any
-}
-
-variable "sql_server" {
-  type = any  
-}
-
-variable "sql_database" {
-  type = any  
-}
-
-variable "key_vaults" {
-  type = any
-}
